f6bdb9910e086cc7d07339e37e34195453a8c1b5
Laura Reale <laaru1988@gmail.com>
2019-03-20 09:56:57 +0100

    add resource validation before charging user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,41 +5,44 @@ class RegistrationsController < Devise::RegistrationsController
     random_password = [*('a'..'z'), *('0'..'9')].sample(6).join
     build_resource(sign_up_params.merge(password: random_password,
 																				temp_password: random_password))
+		if resource.valid?
+			customer = Stripe::Customer.create(
+				email: resource.email,
+				source: stripe_token(params),
+				description: 'The Hub News subscriber'
+			)
 
-    customer = Stripe::Customer.create(
-      email: resource.email,
-      source: stripe_token(params),
-      description: 'The Hub News subscriber'
-    )
+			charge = Stripe::Charge.create(
+				customer: customer.id,
+				amount: 10_000,
+				currency: 'sek',
+				description: '1 month subscription to The Hub News'
+			)
 
-    charge = Stripe::Charge.create(
-      customer: customer.id,
-      amount: 10_000,
-      currency: 'sek',
-      description: '1 month subscription to The Hub News'
-    )
-
-    if charge[:paid]
-      resource.save
-      yield resource if block_given?
-      if resource.persisted?
-        if resource.active_for_authentication?
-          set_flash_message! :notice, :signed_up
-          sign_up(resource_name, resource)
-          respond_with resource, location: after_sign_up_path_for(resource)
-        else
-          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-          expire_data_after_sign_in!
-          respond_with resource, location: after_inactive_sign_up_path_for(resource)
-        end
-      else
-        clean_up_passwords resource
-        set_minimum_password_length
-        respond_with resource
-      end
-    else
-      redirect_to root_path, notice: 'Sorry, something went wrong..'
-    end
+			if charge[:paid]
+				resource.save
+				yield resource if block_given?
+				if resource.persisted?
+					if resource.active_for_authentication?
+						set_flash_message! :notice, :signed_up
+						sign_up(resource_name, resource)
+						respond_with resource, location: after_sign_up_path_for(resource)
+					else
+						set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+						expire_data_after_sign_in!
+						respond_with resource, location: after_inactive_sign_up_path_for(resource)
+					end
+				else
+					clean_up_passwords resource
+					set_minimum_password_length
+					respond_with resource
+				end
+			else
+				redirect_to root_path, notice: 'Sorry, something went wrong..'
+			end
+		else
+			redirect_to new_user_registration_path, notice: "Please insert valid details"
+		end
 	end
 
 	private
