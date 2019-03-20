b4d35f58b6ede5754ab55360f7eabe3a45cf6023
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-03-20 17:15:07 +0100

    removes binding pry

--- app/controllers/staff_content/articles_controller.rb
+++ app/controllers/staff_content/articles_controller.rb
@@ -6,7 +6,6 @@ class StaffContent::ArticlesController < ApplicationController
 	private
 
 	def check_staff_auth
-		binding.pry
 		if user_signed_in? && current_user.role != "journalist"
 			redirect_to root_path
 		end
