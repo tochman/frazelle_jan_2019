1e1265803e864ac543dfeb5087c885c2d3e6c436
Laura Reale <laaru1988@gmail.com>
2019-03-24 09:43:14 +0100

    correct host in mailer for test and development,
    add confirm in seed file for staff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
 	config.action_mailer.perform_caching = false
 
-	config.action_mailer.default_url_options = { host: 'localhost3000' }
+	config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   config.active_support.deprecation = :log
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
 	config.action_mailer.delivery_method = :test
 
-	config.action_mailer.default_url_options = { host: 'localhost3000' }
+	config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
 	config.active_support.deprecation = :stderr
 
