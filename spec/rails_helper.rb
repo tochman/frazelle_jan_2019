5769dd7b85c9e8b287f63bfd7f9853bb41c3139a
Laura Reale <laaru1988@gmail.com>
2019-03-27 13:24:26 +0100

    extract json response to helper method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'spec_helper'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Api::V1::ArticlesController, type: :request do
 		end
 		it 'returns a collection of articles' do
 			get '/api/v1/articles'
-			expect(JSON.parse(response.body)['articles'].count).to eq 5
+			expect(response_json['articles'].count).to eq 5
 		end
 	end
 end
